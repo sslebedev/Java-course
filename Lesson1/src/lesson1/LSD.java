b0b4f9471fc758b5fb07ef58884b8f04e97a1f9e
sslebedev <ssergejslebedev@gmail.com>
2015-09-14 10:30:45 +0300

    LSD sort from Sedgewick's sample; Added result check

--- Lesson1/src/lesson1/LSD.java
+++ Lesson1/src/lesson1/LSD.java
@@ -22,14 +22,54 @@ public class LSD {
         System.out.println("Elapsed = " + (stop - start));
 
         start = System.nanoTime();
-        LSDsort(data);
+        LSDSort(data2);
         stop = System.nanoTime();
 
         System.out.println("Elapsed = " + (stop - start));
+
+        System.out.println("Passed = " + java.util.Arrays.equals(data, data2));
     }
 
-    private static void LSDsort(int[] data) {
-        // TODO Auto-generated method stub
+    private static void LSDSort(int[] data) {
+        int w = 4;
+        int BITS_PER_BYTE = 8;
+        int r = 1 << 8;
+        int mask = 0xFF;
+
+        int n = data.length;
+        int[] aux = new int[n];
+
+        for (int d = 0; d < w; d++) {
+            int[] count = new int[r + 1];
+            for (int i = 0; i < n; i++) {
+                int c = (data[i] >> BITS_PER_BYTE * d) & mask;
+                count[c + 1]++;
+            }
+
+            for (int j = 0; j < r; j++) {
+                count[j + 1] += count[j];
+            }
+
+            if (d == w - 1) {
+                int shift1 = count[r] - count[r / 2];
+                int shift2 = count[r / 2];
+                for (int j = 0; j < r / 2; j++) {
+                    count[j] += shift1;
+                }
+                for (int j = r / 2; j < r; j++) {
+                    count[j] -= shift2;
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                int c = (data[i] >> BITS_PER_BYTE * d) & mask;
+                aux[count[c]++] = data[i];
+            }
+
+            for (int i = 0; i < n; i++){
+                data[i] = aux[i];
+            }
+        }
     }
 
     private static int[] generate() {
