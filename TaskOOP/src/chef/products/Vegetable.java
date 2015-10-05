ce2213f1d24274093b9ff604d32accc08d909516
sslebedev <ssergejslebedev@gmail.com>
2015-10-05 11:10:27 +0300

    Vegetable extends product

---
+++ TaskOOP/src/chef/products/Vegetable.java
@@ -0,0 +1,12 @@
+package chef.products;
+
+import chef.Product;
+
+/**
+ * Created by Сергей on 05.10.2015.
+ */
+public class Vegetable extends Product{
+    public Vegetable(String name, float calories) {
+        super(name, calories);
+    }
+}
--- TaskOOP/src/chef/vegetable.java
+++
@@ -1,7 +0,0 @@
-package chef;
-
-/**
- * Created by Сергей on 05.10.2015.
- */
-public class Vegetable {
-}
