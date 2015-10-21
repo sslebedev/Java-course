93e851558759c5fd9110941223acdea4a217beed
sslebedev <ssergejslebedev@gmail.com>
2015-10-21 07:40:55 +0300

    Acceptable design

--- SimpleCalc/app/src/main/java/com/sslebedev/simplecalc/MainActivity.java
+++ SimpleCalc/app/src/main/java/com/sslebedev/simplecalc/MainActivity.java
@@ -2,6 +2,8 @@ package com.sslebedev.simplecalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.widget.RemoteViews;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
+
 }
